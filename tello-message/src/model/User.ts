3229279a0e3c2be613aa6fc3c5b0fb27087bd2dc
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-07-07 19:24:31 +0530

    create message schema

--- tello-message/src/model/User.ts
+++ tello-message/src/model/User.ts
@@ -2,6 +2,14 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 export interface Message extends Document {
   content: string;
+createdAt: Date;
   timestamp: Date;
 }
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: { type: String, required: true },
+  createdAt: { type: Date, required: true , default: Date.now },
+  timestamp: { type: Date, default: Date.now },
+});
+
 
