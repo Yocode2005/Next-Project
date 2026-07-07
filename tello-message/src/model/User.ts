7949ed540bd210c62c626d5f0df03654e71eb7a5
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-07-07 19:31:44 +0530

    add user schema

--- tello-message/src/model/User.ts
+++ tello-message/src/model/User.ts
@@ -13,11 +13,21 @@ const MessageSchema: Schema<Message> = new Schema({
 });
 
 export interface User extends Document { // schema for the user 
+  username: string;
   email: string;
   password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isAcceptingMessages: boolean;
+  message: Message[];// for the messages sent to the user
 }
 
 const UserSchema: Schema<User> = new Schema({
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  verifyCode: { type: String },
+  verifyCodeExpiry: { type: Date },
+  isAcceptingMessages: { type: Boolean, default: true },
+  message: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
 });
