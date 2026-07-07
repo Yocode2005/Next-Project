251b01d6f39a90b9c4e9ff00ef34ba2018705999
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-07-07 19:46:09 +0530

    define user model

--- tello-message/src/model/User.ts
+++ tello-message/src/model/User.ts
@@ -1,35 +1,39 @@
 import mongoose, { Document, Schema } from "mongoose";
 
-export interface Message extends Document {
-  // schema for the message
+export interface Message extends Document {// schema for the message
   content: string;
   createdAt: Date;
   timestamp: Date;
 }
 
-const MessageSchema: Schema<Message> = new Schema({
+const MessageSchema: Schema<Message> = new Schema({ 
   content: { type: String, required: true },
   createdAt: { type: Date, required: true, default: Date.now },
   timestamp: { type: Date, default: Date.now },
 });
 
-export interface User extends Document {
-  // schema for the user
+export interface User extends Document {// schema for the user
   username: string;
   email: string;
   password: string;
   verifyCode: string;
   verifyCodeExpiry: Date;
+  isVerified: boolean;
   isAcceptingMessages: boolean;
   message: Message[]; // for the messages sent to the user
 }
 
 const UserSchema: Schema<User> = new Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  verifyCode: { type: String },
-  verifyCodeExpiry: { type: Date },
+  username: { type: String, required: [true, "Username is required"], trim: true, unique: true },
+  email: { type: String, required: [true, "Email is required"], unique: true },
+  password: { type: String, required: [true, "Password is required"]},
+  verifyCode: { type: String, required: [true, "Verification code is required"] },
+  verifyCodeExpiry: { type: Date, required: [true, "Verification code expiry is required"] },
+  isVerified: { type: Boolean, default: false },
   isAcceptingMessages: { type: Boolean, default: true },
-  message: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+  message: [MessageSchema], // for the messages sent to the user
 });
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema); // check if the model already exists, if not create a new one
+
+export default UserModel;
